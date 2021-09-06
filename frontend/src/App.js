50de72024596d6a22fbd32f292d632735e71710c
Jorge <jorgeroca1996@gmail.com>
2021-09-06 21:19:18 +0200

    Configuracion de React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,30 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import Category from "./pages/Category";
+
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/post/:id">
+            <Post />
+          </Route>
+          <Route path="/category/:id">
+            <Category />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
