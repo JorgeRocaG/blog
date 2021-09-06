f5f2a39d4b93bc1feebb2a32f9573570502533a3
Jorge <jorgeroca1996@gmail.com>
2021-09-06 22:51:22 +0200

    Obtener el Post individual

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function Post() {
+  const { id } = useParams();
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/posts/" + id
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
     <div>
-      <h1>Post</h1>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
     </div>
   );
 }
