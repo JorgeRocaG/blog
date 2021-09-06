c3c158cf4c8d23b7cbd3f38cc8a39c4ebef20d29
Jorge <jorgeroca1996@gmail.com>
2021-09-06 21:41:53 +0200

    Obtener el listado de posts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import { Link } from "react-router-dom";
 
 export default function Home() {
+  const { loading, error, data } = useFetch("http://localhost:1337/posts");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error! :(</p>;
+
+  console.log(data);
+
   return (
     <div>
       <h1>Home</h1>
+      {data.map((post) => (
+        <div key={post.id} className="post-card">
+          <h2>{post.title}</h2>
+          <p>{post.body.substring(0, 200)}...</p>
+          <Link to={`/post/${post.id}`}>Leer más</Link>
+        </div>
+      ))}
     </div>
   );
 }
